46034ed11a4a31fd489ffc2421032cd90169dc15
saufiroja <saufiroja08@gmail.com>
2022-03-08 16:55:06 +0800

    change test

--- test/user.controllers.spec.js
+++ test/user.controllers.spec.js
@@ -2,41 +2,43 @@ const request = require('supertest');
 const { app } = require('../src/app');
 
 describe('User API', () => {
-  // positive test
-  it('should show all users', async () => {
-    const res = await request(app).get('/api/users');
-    expect(res.statusCode).toBe(200);
-    expect(res.body.message).toBe('success get all user');
-  });
-
-  it('should get user profile', async () => {
-    const res = await request(app).get('/api/user/halo1');
-    expect(res.statusCode).toBe(200);
-    expect(res.body.message).toBe('successfully retrieve data');
-  });
-
-  // negative test
-  it('should get user profile user with error', async () => {
-    const res = await request(app).get('/api/user/99999');
-    expect(res.statusCode).toBe(404);
-    expect(res.body.message).toBe('user not registered');
-  });
-
-  it('should get one by id with error', async () => {
-    const res = await request(app).get(
-      '/api/users/310f067e-9b43-11ec-b909-0242ac120002'
-    );
-    expect(res.statusCode).toBe(404);
-    expect(res.body.message).toBe('user not registered');
-  });
-
-  it('should register with email already exists', async () => {
+  it('[negative] should register with Email already exists', async () => {
     const res = await request(app).post('/api/register').send({
       username: 'halo1',
       email: 'halo1@gmail.com',
       password: '123',
     });
-    expect(res.statusCode).toBe(400);
-    expect(res.body).toBe('Email already exists');
+    expect(res.statusCode).toBe(500);
+  });
+
+  it('[negative] should get user profile user with error', async () => {
+    const res = await request(app).get('/api/user/halo');
+    expect(res.statusCode).toBe(500);
+  });
+
+  it('[negative] should get one by id with error', async () => {
+    const res = await request(app).get(
+      '/api/users/310f067e-9b43-11ec-b909-0242ac120002'
+    );
+    expect(res.statusCode).toBe(500);
+  });
+
+  it('[posotive] should register', async () => {
+    const res = await request(app).post('/api/register').send({
+      username: 'halo1',
+      email: 'halo1@gmail.com',
+      password: '123',
+    });
+    expect(res.statusCode).toBe(201);
+    expect(res.body.message).toBe('User created successfully');
+  });
+
+  it('[posotive] should login', async () => {
+    const res = await request(app).post('/api/login').send({
+      email: 'halo1@gmail.com',
+      password: '123',
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe('User logged in successfully');
   });
 });
